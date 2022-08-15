acb626057019edee48f4048af50678fa3676a87c
artemstax2105 <83162351+artstax2105@users.noreply.github.com>
2022-08-15 16:36:56 +0300

    Update HandTrackingModule.py

--- HandTrackingModule.py
+++ HandTrackingModule.py
@@ -1,54 +1,109 @@
 import cv2
-import win32con
-
-import HandTrackingModule as htm
-import cvzone
-import numpy as np
-import win32api
-import pyautogui
-
 import mediapipe as mp
 import time
 import math
 
+class handDetector():
+	def __init__(self, mode=False, maxHands=2, modelComplexity=1, detectionCon=0.5, trackCon=0.5):
+		self.mode = mode
+		self.maxHands = maxHands
+		self.modelComplexity = modelComplexity
+		self.detectionCon = detectionCon
+		self.trackCon = trackCon
 
-cap = cv2.VideoCapture(0)
-cap.set(3, 1280)
-cap.set(4, 720)
-detector = htm.handDetector(detectionCon=0.8)
-colorR = (255, 0, 255)
+		self.mpHands = mp.solutions.hands
+		self.hands = self.mpHands.Hands(self.mode, self.maxHands, self.modelComplexity, self.detectionCon, self.trackCon)
+		self.mpDraw = mp.solutions.drawing_utils
+		self.tipIds = [4, 8, 12, 16, 20] 
 
-cx, cy, w, h = 1000, 1000, 1920, 1080
-pTime = 0
-cTime = 0
+	def findHands(self, img, draw=True):
+		imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+		self.results = self.hands.process(imgRGB)
+		#print(results.multi_hand_landmarks)
+
+		if self.results.multi_hand_landmarks:
+			for handLms in self.results.multi_hand_landmarks:
+				if draw:
+					self.mpDraw.draw_landmarks(img, handLms, self.mpHands.HAND_CONNECTIONS)
+		return img
+
+	def findPosition(self, img, handNo=0, draw=True):
+		xList = []
+		yList = []
+		bbox = []
+		self.lmList = []
+		if self.results.multi_hand_landmarks:
+			myHand = self.results.multi_hand_landmarks[handNo]
+			for id, lm in enumerate(myHand.landmark):
+				#print(id, lm)
+				h, w, c = img.shape
+				cx, cy = int(lm.x*w), int(lm.y*h)
+				xList.append(cx)
+				yList.append(cy)
+				#print(id, cx, cy)
+				self.lmList.append([id, cx, cy])
+				if draw:
+					cv2.circle(img, (cx, cy), 5, (255,0,255), cv2.FILLED)
+			xmin, xmax = min(xList), max(xList)
+			ymin, ymax = min(yList), max(yList)
+			bbox = xmin, ymin, xmax, ymax
+
+			if draw:
+				cv2.rectangle(img, (bbox[0]-20, bbox[1]-20), (bbox[2]+20, bbox[3]+20), (0, 255, 0), 2)
+		return self.lmList, bbox
+
+	def findDistance(self, p1, p2, img, draw=True):
+		x1, y1 = self.lmList[p1][1], self.lmList[p1][2]
+		x2, y2 = self.lmList[p2][1], self.lmList[p2][2]
+		cx, cy = (x1+x2)//2, (y1+y2)//2
+
+		if draw:
+			cv2.circle(img, (x1,y1), 15, (255,0,255), cv2.FILLED)
+			cv2.circle(img, (x2,y2), 15, (255,0,255), cv2.FILLED)
+			cv2.line(img, (x1,y1), (x2,y2), (255,0,255), 3)
+			cv2.circle(img, (cx,cy), 15, (255,0,255), cv2.FILLED)
+
+		length = math.hypot(x2-x1, y2-y1)
+		return length, img, [x1, y1, x2, y2, cx, cy]
+
+	def fingersUp(self):
+		fingers = []
+
+		# Thumb
+		if self.lmList[self.tipIds[0]][1] < self.lmList[self.tipIds[0]-1][1]:
+			fingers.append(1)
+		else:
+			fingers.append(0)
+
+		# 4 Fingers
+		for id in range(1,5):
+			if self.lmList[self.tipIds[id]][2] < self.lmList[self.tipIds[id]-2][2]:
+				fingers.append(1)
+			else:
+				fingers.append(0)
+		return fingers
+
+def main():
+	pTime = 0
+	cTime = 0
+	cap = cv2.VideoCapture(0)
+	detector = handDetector()
+	while True:
+		success, img = cap.read()
+		img = detector.findHands(img)
+		lmList = detector.findPosition(img)
+		if len(lmList) != 0:
+			print(lmList[1])
+
+		cTime = time.time()
+		fps = 1. / (cTime - pTime)
+		pTime = cTime
+
+		cv2.putText(img, str(int(fps)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+
+		cv2.imshow("Image", img)
+		cv2.waitKey(1)
 
 
-while True:
-    success, img = cap.read()
-    img = cv2.flip(img, 1)
-    dim = (1920, 1080)
-    img = cv2.resize(img, dim)
-    img = detector.findHands(img)
-    lmList, _ = detector.findPosition(img)
-
-
-    if len(lmList) != 0:
-        cursor = lmList[8][1:]  # index finger tip landmark
-        # call the update
-
-        win32api.SetCursorPos((cursor[0], cursor[1]))
-        length, _, _ = detector.findDistance(12, 4, img, draw=False)
-        if length < 40:
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTDOWN, cursor[0], cursor[1], 0, 0)
-        else:
-            win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP, cursor[0], cursor[1], 0, 0)
-
-
-
-
-    cv2.waitKey(1)
-    cTime = time.time()
-    fps = 1. / (cTime - pTime)
-    pTime = cTime
-    print(fps)
-
+if __name__ == "__main__":
+	main()
